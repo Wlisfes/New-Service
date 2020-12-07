488d7df51e7c0fd924f8968882ef3eb857908e5f
情雨随风 <876451336@qq.com>
2020-12-07 21:41:17 +0800

    添加mysql、session配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,36 @@ import { Module, HttpModule } from '@nestjs/common'
 import { AppController } from '@/app.controller'
 import { AppService } from '@/app.service'
 
+//全局依赖模块
+import { ConfigModule } from '@nestjs/config'
+import { SessionModule } from 'nestjs-session'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+//表结构
+import { UserEntity } from '@/entity/user.entity'
+
 @Module({
-	imports: [HttpModule],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRoot(),
+		SessionModule.forRootAsync({
+			useFactory() {
+				return {
+					session: {
+						secret: process.env.SESSIONSECRET,
+						resave: true,
+						saveUninitialized: true,
+						cookie: {
+							httpOnly: true,
+							maxAge: 1800 * 1000 //1800秒
+						}
+					}
+				}
+			}
+		}),
+		TypeOrmModule.forFeature([UserEntity]),
+		HttpModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
