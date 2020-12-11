0d806b63f64e616451c602457c3d8ecaa645543c
情雨随风 <876451336@qq.com>
2020-12-11 21:36:39 +0800

    新增商品表、规格表

---
+++ src/entity/product.entity.ts
@@ -0,0 +1,42 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import { AdminEntity } from '@/entity/admin.entity'
+import { SourceEntity } from '@/entity/source.entity'
+
+@Entity('product')
+export class ProductEntity {
+	@PrimaryGeneratedColumn({ comment: '自增长主键' })
+	id: number
+
+	@Column({ type: 'simple-array', nullable: false, comment: 'banner图片' })
+	banner: string[]
+
+	@Column({ type: 'simple-array', nullable: false, comment: '详情图片图片' })
+	content: string[]
+
+	@Column({ nullable: false, comment: '标题' })
+	title: string
+
+	@Column({ nullable: false, comment: '详情描述' })
+	desc: string
+
+	@Column({ default: 0, comment: '浏览量' })
+	browse: number
+
+	@Column({ default: 0, comment: '优惠价' })
+	price: number
+
+	@Column({ default: 0, comment: '原价' })
+	suprice: number
+
+	@ManyToOne(
+		type => AdminEntity,
+		admin => admin.product
+	)
+	admin: AdminEntity
+
+	@ManyToOne(
+		type => SourceEntity,
+		source => source.product
+	)
+	source: SourceEntity
+}
