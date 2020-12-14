6676c9bb8817465523ea2f98281292454bb1177c
情雨随风 <876451336@qq.com>
2020-12-14 21:44:22 +0800

    封装redis订阅事件

--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -4,22 +4,28 @@ import * as Redis from 'ioredis'
 
 @Injectable()
 export class RedisService {
-	private client: Redis.Redis
-	private observer: Redis.Redis
+	public readonly client: Redis.Redis
+	public readonly subserver: Redis.Redis
+	public readonly subKey: string
 
-	constructor(@Inject(OPTIONS) options: Redis.RedisOptions) {
+	constructor(@Inject(OPTIONS) public readonly options: Redis.RedisOptions) {
 		this.client = new Redis(options)
 		this.client.send_command('config', ['set', 'notify-keyspace-events', 'Ex'])
-		this.observer = new Redis(options)
-		const expired_subKey = `__keyevent@${options.db}__:expired`
-		this.observer.subscribe(expired_subKey, () => {
-			this.observer.on('message', (info, key) => {
-				console.log(key, '失效了')
-			})
-		})
-		// this.client.set('user', 'paker', 'EX', 4) //4秒后失效
+		this.subserver = new Redis(options)
+		this.subKey = `__keyevent@${options.db}__:expired`
 	}
 
+	//订阅
+	public observer(co: Function) {
+		const keyPrefix = this.options.keyPrefix
+		this.subserver.subscribe(this.subKey, () => {
+			this.subserver.on('message', (info, key) => {
+				co && co(key.slice(keyPrefix.length))
+			})
+		})
+	}
+
+	//存储
 	public async setStore(key: string, data: any, seconds?: number) {
 		if (!seconds) {
 			return await this.client.set(key, JSON.stringify(data))
@@ -28,6 +34,7 @@ export class RedisService {
 		}
 	}
 
+	//读取
 	public async getStore(key: string) {
 		const data = await this.client.get(key)
 		if (data) {
